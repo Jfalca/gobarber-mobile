39ba298b4a7f93f8ee80181d831cb045b7f765c1
Jorge Falca <jfalca@live.com>
2020-06-21 18:17:33 -0300

    configuração eslint/prettier/reactotron

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,33 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+    "env": {
+        "es2020": true
+    },
+    "extends": [
+        "plugin:react/recommended",
+        "airbnb",
+        'prettier',
+        'prettier/react'
+    ],
+    globals:{
+      Atomics:'readonly',
+      SharedArrayBuffer: 'readonly',
+      __DEV__: 'readonly'
+    },
+    parser:'babel-eslint',
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 11,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        'prettier'
+    ],
+    "rules": {
+      'prettier/prettier': 'error',
+      'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
+      'import/prefer-default-export': 'off',
+    }
 };
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import './config/ReactotronConfig';
+
+// import { Container } from './styles';
+
+export default function App() {
+  return (
+    <View>
+      <Text>Ola dentro do index</Text>
+    </View>
+  );
+}
